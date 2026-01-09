e3f954ba1c24062bea60b5ebcbf27faa13b4412d
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-10 01:12:31 +0530

    Settings up page routes with link.
    Here, when you change the URL then it routes to that particular page.jsx inside pages.
    Example:
    <Route path="/agence" element={<Agence />} />
    URL: http://localhost:5173/agence
    Page: src\pages\Agence.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='text-6xl font-bold text-white'>App</div>
+    <div className="text-white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/agence" element={<Agence />} />
+        <Route path="projects" element={<Projects />} />
+      </Routes>
+    </div>
   )
 }
 
