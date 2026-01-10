cdab6b01651648a84307f4dff656cbe638fd7a0f
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-10 16:48:46 +0530

    Add the video component in home.
    Add respective css as needed and import the video component.

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Video = () => {
   return (
-    <div>Video</div>
+    <div>
+      <video autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1768040450~exp=1768044050~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=d58b6761e34169a38487b2cf0f8ce46bf66a2b6a6c38aa825ccf95f32fce1cd5&r=dXMtZWFzdDE%3D" />
+    </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Video } from '../components/home/Video'
 
 const Home = () => {
     return (
