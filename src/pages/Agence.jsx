1ff3c0075e06c0859b94dff13cb365573c6532e3
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-11 00:24:26 +0530

    Add the main text and subtext.
    Hello&ensp;World   <!-- small -->
    Hello&emsp;World   <!-- tab-like -->
    &nbsp; actually means non-breaking space, and 1 &nbsp; = 1 space only.

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Agence = () => {
     return (
-        <div>Agence</div>
+        <div className="font-[font2] text-white">
+            <div className="bg-red-700 mt-[55vh]">
+                <h1 className="text-[19vw] leading-[19vw] text-center uppercase">
+                    SEVEN7Y <br /> TWO
+                </h1>
+            </div>
+            <div className="bg-blue-700 pl-[40%] mt-5">
+                <p className="text-2xl leading-[1em]"> &nbsp; &emsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+            </div>
+        </div>
     )
 }
 
