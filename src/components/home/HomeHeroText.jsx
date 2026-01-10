f0d03d8b410dc1bff3facc158a858c8de9af1324
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-10 20:18:11 +0530

    Add the text for hero sections.

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const HomeHeroText = () => {
   return (
-    <div>HomeHeroText</div>
+    <div className='text-[9.5vw] font-[font1] text-center'>
+      <div className="text-white uppercase leading-[9vw]">The spark for</div>
+      <div className="text-white uppercase leading-[9vw]">allthings</div>
+      <div className="text-white uppercase leading-[9vw]">creative</div>
+    </div>
   )
 }
