7805b226559f6fab16241bdcfc69c6ed92d84315
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-10 23:46:46 +0530

    Style home bottom text.
    Note: One is done by using tailwindcss directly inside Link and the other(Agence) is contained inside other div.
    It shows different approach.

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const HomeBottomText = () => {
   return (
-    <div>HomeBottomText</div>
+    <div className="font-[font2] text-[9vw] text-center text-white uppercase flex items-center justify-center gap-1 mb-1">
+      <Link to="/projects" className="border-2 rounded-full leading-[9vw] px-4 -py-2 pt-1.5 hover:text-[#d3fd50]">
+        Projects
+      </Link>
+      <div className="border-2 rounded-full leading-[9vw] px-4 py-1 hover:text-[#d3fd50]">
+        <Link to="/agence" className="text-[9vw] hover:text-[#d3fd50]">
+          Agence
+        </Link>
+      </div>
+    </div>
   )
 }
