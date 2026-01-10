437bb4929beb60fb1e3c7985e70dc672eda86449
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-10 19:46:56 +0530

    only a recommendation, not an error.
    Uses @vitejs/plugin-react-swc
    
    ❌ But you are not using any SWC-specific plugins

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -9,3 +9,4 @@ export default defineConfig({
     tailwindcss(),
   ],
 })
+
