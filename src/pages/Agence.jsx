8af5937068b7f4279b12f8c1128b1e7e594d70c2
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-11 14:36:47 +0530

    Add hero text and paragraph for agence page.

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 const Agence = () => {
     return (
-        <div className="font-[font2] text-white">
-            <div className="bg-red-700 mt-[55vh]">
-                <h1 className="text-[19vw] leading-[19vw] text-center uppercase">
-                    SEVEN7Y <br /> TWO
-                </h1>
+        <div>
             </div>
-            <div className="bg-blue-700 pl-[40%] mt-5">
-                <p className="text-2xl leading-[1em]"> &nbsp; &emsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+            <div className="font-[font2] text-white">
+                <div className="bg-red-700 mt-[55vh]">
+                    <h1 className="text-[19vw] leading-[19vw] text-center uppercase">
+                        SEVEN7Y <br /> TWO
+                    </h1>
+                </div>
+                <div className="bg-blue-700 pl-[40%] mt-5">
+                    <p className="text-2xl leading-[1em]"> &nbsp; &emsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+                </div>
             </div>
         </div>
     )
