81016296af8e4514c76068c62732d4d5e3af8b4e
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-11 14:49:20 +0530

    Divide the structure into sections.
    It is standard approach to handle pages. Section2 h-screen allows for complete new page when scrolled down, only black bg appears or as set by the user. Add img tag as well with overflow hidden and other w-full and h-full.

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -3,18 +3,23 @@ import React from 'react'
 const Agence = () => {
     return (
         <div>
-            <div className="absolute h-[30vw] w-[23vw] top-55 left-[23vw] rounded-4xl bg-yellow-500">
-
+            <div className="section1">
+                <div className="absolute overflow-hidden h-[30vw] w-[23vw] top-55 left-[23vw] rounded-4xl bg-yellow-500">
+                    <img src="public/images/download.jpg" alt="luffy" className="w-full h-full object-cover" />
+                </div>
+                <div className="font-[font2] text-white relative">
+                    <div className="mt-[55vh]">
+                        <h1 className="text-[19vw] leading-[19vw] text-center uppercase">
+                            SEVEN7Y <br /> TWO
+                        </h1>
+                    </div>
+                    <div className="bg-blue-700 pl-[40%] mt-5">
+                        <p className="text-2xl leading-[1em]"> &nbsp; &emsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+                    </div>
+                </div>
             </div>
-            <div className="font-[font2] text-white relative">
-                <div className="mt-[55vh]">
-                    <h1 className="text-[19vw] leading-[19vw] text-center uppercase">
-                        SEVEN7Y <br /> TWO
-                    </h1>
-                </div>
-                <div className="bg-blue-700 pl-[40%] mt-5">
-                    <p className="text-2xl leading-[1em]"> &nbsp; &emsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
-                </div>
+            <div className="section2 h-screen">
+
             </div>
         </div>
     )
