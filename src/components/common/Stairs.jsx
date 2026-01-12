bb9092c898b00c765a28b65d3d28487e3f103078
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-13 00:15:47 +0530

    Add path dependency for stairs.
    Use Stairs component in the main.jsx so that it runs everytime the App is loaded.

--- src/components/common/Stairs.jsx
+++ src/components/common/Stairs.jsx
@@ -1,9 +1,13 @@
 import { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { useLocation } from 'react-router-dom'
 
 const Stairs = () => {
-      const stairParentRef = useRef(null);
+
+    const currentPath = useLocation().pathname;
+    const stairParentRef = useRef(null);
+
   useGSAP(function () {
     const tl = gsap.timeline();
     tl.to(stairParentRef.current, {
@@ -27,12 +31,12 @@ const Stairs = () => {
     tl.to('.stair', {
       y: '0%',
     })
-  })
+  }, [currentPath])
     return (
         <div ref={stairParentRef} className="h-screen w-screen fixed z-10 top-0">
             <div className="h-full w-full flex">
                 <div className="stair h-full w-1/5 bg-black" />
-                <div className="stair h-full w-1/5 bg-black" />
+                <div className="stair h-full w-1/4 bg-black" />
                 <div className="stair h-full w-1/5 bg-black" />
                 <div className="stair h-full w-1/5 bg-black" />
                 <div className="stair h-full w-1/5 bg-black" />
