d42822542b4e215c93119035c4245ae59d723871
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-12 14:17:34 +0530

    Initial stair animation from top to bottom.

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,27 @@ import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import { Route, Routes } from 'react-router-dom'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const App = () => {
+  useGSAP(function () {
+    gsap.from('.stair', {
+      height: 0,
+      stagger: {
+        amount: -0.3
+      }
+    })
+  })
+
   return (
     <div className="text-white">
       <div className="h-screen w-full flex fixed z-10 top-0">
-        <div className="h-full w-1/5 bg-black" />
-        <div className="h-full w-1/5 bg-white" />
-        <div className="h-full w-1/5 bg-black" />
-        <div className="h-full w-1/5 bg-white" />
-        <div className="h-full w-1/5 bg-black" />
+        <div className="stair h-full w-1/5 bg-black" />
+        <div className="stair h-full w-1/5 bg-white" />
+        <div className="stair h-full w-1/5 bg-black" />
+        <div className="stair h-full w-1/5 bg-white" />
+        <div className="stair h-full w-1/5 bg-black" />
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
