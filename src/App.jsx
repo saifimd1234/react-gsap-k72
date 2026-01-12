e146bce89f9d06ae4c9cd4197cfa3c596e8c31bb
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-12 14:33:55 +0530

    Smoothen the stairs animation.

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,49 @@ import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import { Route, Routes } from 'react-router-dom'
+import { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 
 const App = () => {
+  const stairParentRef = useRef(null);
   useGSAP(function () {
-    gsap.from('.stair', {
+    const tl = gsap.timeline();
+    tl.to(stairParentRef.current, {
+      display: 'block',
+    })
+    tl.from('.stair', {
       height: 0,
+      stagger: {
+        amount: -0.25
+      }
+    })
+    tl.to('.stair', {
+      y: '100%',
       stagger: {
         amount: -0.3
       }
+    })
+    tl.to(stairParentRef.current, {
+      display: 'none',
+    })
+    tl.to('.stair', {
+      y: '0%',
     })
   })
 
   return (
     <div className="text-white">
-      <div className="h-screen w-full flex fixed z-10 top-0">
-        <div className="stair h-full w-1/5 bg-black" />
-        <div className="stair h-full w-1/5 bg-white" />
-        <div className="stair h-full w-1/5 bg-black" />
-        <div className="stair h-full w-1/5 bg-white" />
-        <div className="stair h-full w-1/5 bg-black" />
+      <div ref={stairParentRef} className="h-screen w-screen fixed z-10 top-0">
+        <div className="h-full w-full flex">
+          <div className="stair h-full w-1/5 bg-black" />
+          <div className="stair h-full w-1/5 bg-black" />
+          <div className="stair h-full w-1/5 bg-black" />
+          <div className="stair h-full w-1/5 bg-black" />
+          <div className="stair h-full w-1/5 bg-black" />
+        </div>
       </div>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agence" element={<Agence />} />
