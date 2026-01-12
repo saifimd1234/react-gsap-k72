15c47a809fc9a6677ac58e9d020b6bc1819195dc
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-13 02:14:59 +0530

    Fixing homeherotext size.

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -3,18 +3,18 @@ import { Video } from './Video'
 
 export const HomeHeroText = () => {
   return (
-    <div className='font-[font1] text-[9.5vw] text-center'>
-      <div className="text-white uppercase leading-[9vw] pt-[10vw] flex items-center justify-center">
+    <div className='font-[font1] text-[12vw] text-center'>
+      <div className="text-white uppercase leading-[11vw] pt-[20vw] flex items-center justify-center">
         The spark for
       </div>
-      <div className="text-white uppercase leading-[9vw] flex items-center justify-center">
+      <div className="text-white uppercase leading-[11vw] flex items-center justify-center">
         all
         <div className="h-[7vw] w-[16vw] -mt-2 rounded-full overflow-hidden">
           <Video />
         </div>
         things
       </div>
-      <div className="text-white uppercase leading-[9vw] flex items-center justify-center">
+      <div className="text-white uppercase leading-[11vw] flex items-center justify-center">
         creative
       </div>
     </div>
