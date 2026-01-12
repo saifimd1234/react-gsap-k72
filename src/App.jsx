e2e9a083e3f5c656e60f86b2f37c1ed4bc952b8d
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-13 02:06:33 +0530

    Set logo svg and add Navbar component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/navigation/Navbar'
 
 const App = () => {
 
 
   return (
     <div className="text-white">
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agence" element={<Agence />} />
