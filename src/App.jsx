79523adc338b9a1e3fd3a92c319be3f0c80b1679
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-12 14:37:12 +0530

    Destructing the code to make stairs as common component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,12 @@ import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import { Route, Routes } from 'react-router-dom'
-import { useRef } from 'react'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
 
 const App = () => {
-  const stairParentRef = useRef(null);
-  useGSAP(function () {
-    const tl = gsap.timeline();
-    tl.to(stairParentRef.current, {
-      display: 'block',
-    })
-    tl.from('.stair', {
-      height: 0,
-      stagger: {
-        amount: -0.25
-      }
-    })
-    tl.to('.stair', {
-      y: '100%',
-      stagger: {
-        amount: -0.3
-      }
-    })
-    tl.to(stairParentRef.current, {
-      display: 'none',
-    })
-    tl.to('.stair', {
-      y: '0%',
-    })
-  })
+
 
   return (
     <div className="text-white">
-      <div ref={stairParentRef} className="h-screen w-screen fixed z-10 top-0">
-        <div className="h-full w-full flex">
-          <div className="stair h-full w-1/5 bg-black" />
-          <div className="stair h-full w-1/5 bg-black" />
-          <div className="stair h-full w-1/5 bg-black" />
-          <div className="stair h-full w-1/5 bg-black" />
-          <div className="stair h-full w-1/5 bg-black" />
-        </div>
-      </div>
 
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ src/components/common/Stairs.jsx
@@ -0,0 +1,44 @@
+import { useRef } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+const Stairs = () => {
+      const stairParentRef = useRef(null);
+  useGSAP(function () {
+    const tl = gsap.timeline();
+    tl.to(stairParentRef.current, {
+      display: 'block',
+    })
+    tl.from('.stair', {
+      height: 0,
+      stagger: {
+        amount: -0.25
+      }
+    })
+    tl.to('.stair', {
+      y: '100%',
+      stagger: {
+        amount: -0.3
+      }
+    })
+    tl.to(stairParentRef.current, {
+      display: 'none',
+    })
+    tl.to('.stair', {
+      y: '0%',
+    })
+  })
+    return (
+        <div ref={stairParentRef} className="h-screen w-screen fixed z-10 top-0">
+            <div className="h-full w-full flex">
+                <div className="stair h-full w-1/5 bg-black" />
+                <div className="stair h-full w-1/5 bg-black" />
+                <div className="stair h-full w-1/5 bg-black" />
+                <div className="stair h-full w-1/5 bg-black" />
+                <div className="stair h-full w-1/5 bg-black" />
+            </div>
+        </div>
+    )
+}
+
+export default Stairs
