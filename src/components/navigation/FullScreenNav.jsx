c091a8872d8c1ec967da8f48ee7fa95328bc6440
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-13 16:18:46 +0530

    Fixing the text and other css properties.
    The return 3rd div has been completely copied below to make another navbar option.

--- src/components/navigation/FullScreenNav.jsx
+++ src/components/navigation/FullScreenNav.jsx
@@ -4,9 +4,9 @@ const FullScreenNav = () => {
     return (
         <div id='fullscreennav' className="text-white h-screen py-40 w-full absolute bg-amber-900">
             <div id='all-links' className=''>
-                <div className='link border-y border-white'>
-                    <h1 className='font-font[2] text-[8vw] uppercase'>Projects</h1>
-                    <div>
+                <div className='link border-t-[0.3px] border-white'>
+                    <h1 className='font-font[2] text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>Projects</h1>
+                    {/* <div>
                         <div>
                             <h2>See Everything</h2>
                             <img src='public\images\luffy0.jpg' alt='' />
@@ -19,7 +19,24 @@ const FullScreenNav = () => {
                             <h2>See Everything</h2>
                             <img src='public\images\luffy1.png' alt='' />
                         </div>
-                    </div>
+                    </div> */}
+                </div>
+                <div className='link border-t-[0.3px] border-white'>
+                    <h1 className='font-font[2] text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>Agency</h1>
+                    {/* <div>
+                        <div>
+                            <h2>See Everything</h2>
+                            <img src='public\images\luffy0.jpg' alt='' />
+                            <h2>See Everything</h2>
+                            <img src='public\images\luffy1.png' alt='' />
+                        </div>
+                        <div>
+                            <h2>See Everything</h2>
+                            <img src='public\images\luffy0.jpg' alt='' />
+                            <h2>See Everything</h2>
+                            <img src='public\images\luffy1.png' alt='' />
+                        </div>
+                    </div> */}
                 </div>
             </div>
         </div>
