fe42b38e260d0d1cc2c1b061a3517d6e95c9def5
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-14 15:03:42 +0530

    Make 4 wrapper for the FullScreenNavbar.
    The className=link is one div and similarly there are 4 divs.

--- src/components/navigation/FullScreenNav.jsx
+++ src/components/navigation/FullScreenNav.jsx
@@ -6,7 +6,7 @@ const FullScreenNav = () => {
             <div id='all-links' className=''>
                 <div className='link relative border-t-[0.3px] border-white'>
                     <h1 className='font-font[2] text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>Projects</h1>
-                    <div className='absolute w-full left-0 top-0 overflow text-black bg-[#D3FD50]'>
+                    <div className='moveLink absolute w-full left-0 top-0 overflow text-black bg-[#D3FD50]'>
                         <div className='flex w-max'>
                             <div className='moveX flex items-center'>
                                 <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
@@ -23,23 +23,64 @@ const FullScreenNav = () => {
                         </div>
                     </div>
                 </div>
-                {/* <div className='link border-t-[0.3px] border-white'>
-                    <h1 className='font-font[2] text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>Agency</h1>
-                    <div>
-                        <div>
-                            <h2>See Everything</h2>
-                            <img src='public\images\luffy0.jpg' alt='' />
-                            <h2>See Everything</h2>
-                            <img src='public\images\luffy1.png' alt='' />
-                        </div>
-                        <div>
-                            <h2>See Everything</h2>
-                            <img src='public\images\luffy0.jpg' alt='' />
-                            <h2>See Everything</h2>
-                            <img src='public\images\luffy1.png' alt='' />
+                <div className='link relative border-t-[0.3px] border-white'>
+                    <h1 className='font-font[2] text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>Projects</h1>
+                    <div className='moveLink absolute w-full left-0 top-0 overflow text-black bg-[#D3FD50]'>
+                        <div className='flex w-max'>
+                            <div className='moveX flex items-center'>
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy0.jpg' alt='' />
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy1.png' alt='' />
+                            </div>
+                            <div className='moveX flex items-center'>
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy0.jpg' alt='' />
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy1.png' alt='' />
+                            </div>
                         </div>
                     </div>
-                </div> */}
+                </div>
+                <div className='link relative border-t-[0.3px] border-white'>
+                    <h1 className='font-font[2] text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>Projects</h1>
+                    <div className='moveLink absolute w-full left-0 top-0 overflow text-black bg-[#D3FD50]'>
+                        <div className='flex w-max'>
+                            <div className='moveX flex items-center'>
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy0.jpg' alt='' />
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy1.png' alt='' />
+                            </div>
+                            <div className='moveX flex items-center'>
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy0.jpg' alt='' />
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy1.png' alt='' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='link relative border-t-[0.3px] border-white'>
+                    <h1 className='font-font[2] text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>Projects</h1>
+                    <div className='moveLink absolute w-full left-0 top-0 overflow text-black bg-[#D3FD50]'>
+                        <div className='flex w-max'>
+                            <div className='moveX flex items-center'>
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy0.jpg' alt='' />
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy1.png' alt='' />
+                            </div>
+                            <div className='moveX flex items-center'>
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy0.jpg' alt='' />
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy1.png' alt='' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div >
     )
