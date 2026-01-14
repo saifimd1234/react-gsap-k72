ad50a7d48d088c61dd0800ca3facc100195646fb
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-14 14:59:50 +0530

    Animate the wrapper and fix overlap.
    Remove flex from the absolute parent
    
    Add a single flex wrapper around the two .moveX blocks
    
    Add overflow-hidden to clip overflow

--- src/components/navigation/FullScreenNav.jsx
+++ src/components/navigation/FullScreenNav.jsx
@@ -4,21 +4,23 @@ const FullScreenNav = () => {
     return (
         <div id='fullscreennav' className="text-white h-screen py-40 w-full absolute bg-amber-900">
             <div id='all-links' className=''>
-                <div className='relative border-t-[0.3px] border-white'>
+                <div className='link relative border-t-[0.3px] border-white'>
                     <h1 className='font-font[2] text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>Projects</h1>
-                    <div className='absolute flex top-0 text-black bg-[#D3FD50]'>
-                        <div className='flex items-center overflow-auto'>
-                            <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
-                            <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy0.jpg' alt='' />
-                            <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
-                            <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy1.png' alt='' />
+                    <div className='absolute w-full left-0 top-0 overflow text-black bg-[#D3FD50]'>
+                        <div className='flex w-max'>
+                            <div className='moveX flex items-center'>
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy0.jpg' alt='' />
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy1.png' alt='' />
+                            </div>
+                            <div className='moveX flex items-center'>
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy0.jpg' alt='' />
+                                <h2 className='font-font[2] whitespace-nowrap text-center text-[10vw] leading-[1.1] -mt-2 uppercase'>See Everything</h2>
+                                <img className='px-2 h-24 rounded-full shrink-0 w-96 object-cover' src='public\images\luffy1.png' alt='' />
+                            </div>
                         </div>
-                        {/* <div>
-                            <h2>See Everything</h2>
-                            <img src='public\images\luffy0.jpg' alt='' />
-                            <h2>See Everything</h2>
-                            <img src='public\images\luffy1.png' alt='' />
-                        </div> */}
                     </div>
                 </div>
                 {/* <div className='link border-t-[0.3px] border-white'>
@@ -39,7 +41,7 @@ const FullScreenNav = () => {
                     </div>
                 </div> */}
             </div>
-        </div>
+        </div >
     )
 }
 
